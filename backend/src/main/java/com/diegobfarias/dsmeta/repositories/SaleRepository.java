4d88a0c6eec8bbb5d393d1c6464787b5c00cfcca
Diego Farias <fariasdb@outlook.com>
2022-09-10 11:36:26 -0300

    Date selection

--- backend/src/main/java/com/diegobfarias/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/diegobfarias/dsmeta/repositories/SaleRepository.java
@@ -1,9 +1,16 @@
 package com.diegobfarias.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.diegobfarias.dsmeta.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+	@Query("SELECT obj FROM Sale obj WHERE obj.date BETWEEN :minLocalDate AND :maxLocalDate ORDER BY obj.amount DESC")
+	Page<Sale> findSales(LocalDate minLocalDate, LocalDate maxLocalDate, Pageable pageable);
 }
--- backend/src/main/java/com/diegobfarias/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/diegobfarias/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.diegobfarias.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.diegobfarias.dsmeta.entities.Sale;
@@ -14,7 +18,13 @@ public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
 
-	public List<Sale> findSales() {
-		return saleRepository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate minLocalDate = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+		LocalDate maxLocalDate = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return saleRepository.findSales(minLocalDate, maxLocalDate, pageable);
 	}
 }
