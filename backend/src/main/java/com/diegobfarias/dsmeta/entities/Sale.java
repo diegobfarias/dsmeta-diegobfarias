f705288156d99908d61eadec2cb9fa5079820076
Diego Farias <fariasdb@outlook.com>
2022-09-10 11:03:34 -0300

    Database

---
+++ backend/src/main/java/com/diegobfarias/dsmeta/entities/Sale.java
@@ -0,0 +1,73 @@
+package com.diegobfarias.dsmeta.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Float amount;
+	private LocalDate date;
+	
+	public Sale() {
+	}
+	
+	public Sale(Long id, String sellerName, Integer visited, Integer deals, Float amount, LocalDate date) {
+		this.id = id;
+		this.sellerName = sellerName;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getSellerName() {
+		return sellerName;
+	}
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+	public Integer getVisited() {
+		return visited;
+	}
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+	public Integer getDeal() {
+		return deals;
+	}
+	public void setDeal(Integer deals) {
+		this.deals = deals;
+	}
+	public Float getAmount() {
+		return amount;
+	}
+	public void setAmount(Float amount) {
+		this.amount = amount;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+}
